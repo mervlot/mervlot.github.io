b9be6176c526d6cd11a50b697771b01592dfb914
Ogunmuyiwa Muhammad <princeadeite09@gmail.com>
2025-03-10 08:23:33 +0100

    1.0

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).on('load', function () {
+  $('#preloader').delay(1000).fadeOut('slow');
+});
+
+
+
+  setTimeout(function () {
+    $(".head").fadeIn();
+    $(".slider").fadeIn();
+  }, 1000);
+
+
